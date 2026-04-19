31a35e4a2861eab64beca1c4cf889bb717952526
ტანჯულა <yleshavi69@gmail.com>
2026-04-19 21:55:17 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,23 @@
 import discord
+from flask import Flask
+from threading import Thread
+import os
 
-# თუ ეს ტოკენი არ იმუშავებს, გამოიყენე Reset Token და ჩასვი ახალი აქ:
-TOKEN = 'MTQ5NTQ3MDk4MTEyNzQ3MTIyNA.GAIOv8.MHUimVGIr3M1e2zpW2Iu2y5_NgWEfbfNCOWBRM'
+# 1. Render-ისთვის საჭირო ვებ-სერვერი
+app = Flask('')
 
+@app.route('/')
+def home():
+    return "Maiko is alive and watching!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+# 2. ბოტის ძირითადი ნაწილი
 intents = discord.Intents.default()
 intents.message_content = True 
 
@@ -14,11 +29,13 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    # ბოტმა საკუთარ თავზე არ უპასუხოს
     if message.author == client.user:
         return
 
     msg = message.content.lower()
 
+    # პასუხები ცუდ სიტყვებზე
     if "shit" in msg:
         await message.reply("shit კიარა შენივე shit არ გაჭამო უზრდელი ესს 😒")
     
@@ -31,4 +48,8 @@ async def on_message(message):
     elif "mogityan" in msg:
         await message.reply("ES RA ARISSSSSSSSSSSSSSSS TUTTTTTTTTTTTT")
 
+# 3. ბოტის გაშვება
+keep_alive()
+# ჩასვი შენი ტოკენი ქვემოთ ბრჭყალებში
+TOKEN = "MTQ5NTQ3MDk4MTEyNzQ3MTIyNA.GAIOv8.MHUimVGIr3M1e2zpW2Iu2y5_NgWEfbfNCOWBRM"
 client.run(TOKEN)
